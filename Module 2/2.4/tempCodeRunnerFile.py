de4e53b474711be8b5afec626906c4cf6205c2b1
AaronGWang <aarongwang2008@gmail.com>
2025-07-08 17:43:11 -0400

    deleted unused file

--- Module 2/2.4/tempCodeRunnerFile.py
+++
@@ -1,7 +0,0 @@
-  file = open("cyclopeptide_scoring.txt", "r").readlines()
-  peptide = file[0].strip()
-  experimental_spectrum = list(map(int, file[1].strip().split()))
-  print(len(peptide), len(experimental_spectrum))
-
-  print(cyclopeptide_scoring(peptide, experimental_spectrum))
-  pyperclip.copy(str(cyclopeptide_scoring(peptide, experimental_spectrum)))
